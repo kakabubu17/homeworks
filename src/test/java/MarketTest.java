3ebcfdd0c280ea89c1a82cc993da035e5ac970bf
Дарья Буртовая <gottokinbox@gmail.com>
2020-10-27 20:01:59 +0300

    'второй'

--- src/test/java/MarketTest.java
+++ src/test/java/MarketTest.java
@@ -6,15 +6,13 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.ElementClickInterceptedException;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 
@@ -30,6 +28,7 @@ public class MarketTest {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
         logger.info("driver initialized");
         driver.get("https://market.yandex.ru/");
         logger.info("open page");
@@ -41,21 +40,32 @@ public class MarketTest {
     {
         By menuElectronic = By.xpath("//span[.='Электроника']");
         By menuSmartphones = By.xpath("//a[.='Смартфоны']");
-        By filterSamsung = By.xpath("//span[.='Samsung']/ancestor::li//a");
-        By filterXiaomi = By.xpath("//span[.='Xiaomi']/ancestor::li//a");
+        By filterSamsung = By.xpath("//span[.='Samsung']/..");
+                //By.xpath("//span[.='Samsung']/ancestor::label/input");
+        By filterXiaomi = By.xpath("//span[.='Xiaomi']/..");
         By sortPrice = By.xpath("//button[.='по цене']");
 
         /*new FluentWait<WebDriver>(driver).withTimeout(10, TimeUnit.SECONDS)
-                .pollingEvery(500, TimeUnit.MILLISECONDS)
+                .pollingEvery(1, TimeUnit.MILLISECONDS)
                 .ignoring(ElementClickInterceptedException.class); */
 
-        Thread.sleep(10000);
+        new WebDriverWait(driver, 10).until(ExpectedConditions.invisibilityOf
+                (driver.findElement(By.xpath("//*[contains(text(), 'Напишите, какой товар вам нужен')]"))));
         driver.findElement(menuElectronic).click();
         elementClick(menuSmartphones);
         logger.info("open smartphones");
         addFilterAndSort(filterSamsung, filterXiaomi, sortPrice);
         logger.info("add filters and sort");
+        List<WebElement> phones =  driver.findElements(By.xpath(""));
+        //два листа с самсунгами и сяоми и взять первый элемент из каждого
+        for (WebElement element:phones)
+        {
+            boolean sams = element.getText().startsWith("Смартфон Samsung");
 
+            if (sams = true) element.click(); //добавляем первый самсунг
+
+        }
+        //не забыть удалить телефоны из сравнения в конце
     }
 
 
@@ -66,17 +76,18 @@ public class MarketTest {
         logger.info("close driver");
     }
 
-    private void addFilterAndSort(By filter, By filter2, By sort)
+    private void addFilterAndSort(By filter, By filter2, By sort) throws InterruptedException
     {
         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", driver.findElement(filter));
-        elementClick(filter);
-        elementClick(filter2);
+        driver.findElement(filter).click();
+        driver.findElement(filter2).click();
         elementClick(sort);
+        Thread.sleep(10000);
     }
 
     private void elementClick(By element)
     {
-        new WebDriverWait(driver, 15).until(ExpectedConditions.visibilityOf(driver.findElement(element)));
+        new WebDriverWait(driver, 15).until(ExpectedConditions.elementToBeClickable(driver.findElement(element)));
         driver.findElement(element).click();
     }
 }
